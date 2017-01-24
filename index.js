0cb9467cbc1f7f78d3c331756dc490fe50a3ec64
Sven Anders Robbestad <sven@robbestad.com>
2017-01-24 08:45:45 +0100

    minor changes to html

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-	const Hapi = require('hapi');
+const Hapi = require('hapi');
 const path = require('path');
 const Inert = require('inert');
 const curl = require('curlrequest');
@@ -16,153 +16,153 @@ const index = !isProd ? './index.html' : './dist/index.html';
 const plugins = [];
 
 if (!isProd) {
-	console.log('*** Adding webpack hot/dev middleware');
-	plugins.push({
-			register: require('hapi-webpack-dev-middleware'),
-			options: {
-				config: require(path.join(__dirname, 'config', 'webpack.config.js')),
-				options: {
-					noInfo: true
-				}
-			}
-		},
-		{
-			register: require('hapi-webpack-hot-middleware')
-		});
+  console.log('*** Adding webpack hot/dev middleware');
+  plugins.push({
+      register: require('hapi-webpack-dev-middleware'),
+      options: {
+        config: require(path.join(__dirname, 'config', 'webpack.config.js')),
+        options: {
+          noInfo: true
+        }
+      }
+    },
+    {
+      register: require('hapi-webpack-hot-middleware')
+    });
 }
 
 // static paths
 server.register(Inert, () => {
-	server.route({
-		method: 'GET',
-		path: '/assets/{param*}',
-		handler: {
-			directory: {
-				path: !isProd ? './assets' : './dist/assets',
-				redirectToSlash: true,
-				index: false
-			}
-		}
-	});
+  server.route({
+    method: 'GET',
+    path: '/assets/{param*}',
+    handler: {
+      directory: {
+        path: !isProd ? './assets' : './dist/assets',
+        redirectToSlash: true,
+        index: false
+      }
+    }
+  });
 });
 
 const getData = (input) => {
-	let request;
-	request = new Request('http://localhost:9200/norgerundt/_search', {
-		method: 'POST',
-		body: JSON.stringify({
-			size: 30,
-			query: {
-				query_string: {
-					query: input,
-					default_operator: "AND"
-				}
-			}
-		}),
-		headers: new Headers({
-			'Accept': 'application/json, text/plain, */*',
-			'Content-Type': 'application/json',
-			Authorization: `Basic ZWxhc3RpYzpjaGFuZ2VtZQ==`
-		})
-	});
+  let request;
+  request = new Request('http://localhost:9200/norgerundt/_search', {
+    method: 'POST',
+    body: JSON.stringify({
+      size: 30,
+      query: {
+        query_string: {
+          query: input,
+          default_operator: "AND"
+        }
+      }
+    }),
+    headers: new Headers({
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+      Authorization: `Basic ZWxhc3RpYzpjaGFuZ2VtZQ==`
+    })
+  });
 
-	return fetch(request)
-		.then(res => res.json());
+  return fetch(request)
+    .then(res => res.json());
 };
 
 const getAc = (input) => {
-	let val = input;
-	val = val.replace(':', '\\:');
-	val = val.replace('.', '\\.');
-	val = val.replace(',', '\\,');
+  let val = input;
+  val = val.replace(':', '\\:');
+  val = val.replace('.', '\\.');
+  val = val.replace(',', '\\,');
 
-	let request;
-	request = new Request('http://localhost:9200/norgerundt_autocomplete/_search', {
-		method: 'POST',
-		body: JSON.stringify({
-			size: 5,
-			query: {
-				query_string: {
-					query: `${val}*`,
-					default_operator: "AND"
-				}
-			}
-		}),
-		headers: new Headers({
-			'Accept': 'application/json, text/plain, */*',
-			'Content-Type': 'application/json',
-			Authorization: `Basic ZWxhc3RpYzpjaGFuZ2VtZQ==`
-		})
-	});
+  let request;
+  request = new Request('http://localhost:9200/norgerundt_autocomplete/_search', {
+    method: 'POST',
+    body: JSON.stringify({
+      size: 5,
+      query: {
+        query_string: {
+          query: `${val}*`,
+          default_operator: "AND"
+        }
+      }
+    }),
+    headers: new Headers({
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+      Authorization: `Basic ZWxhc3RpYzpjaGFuZ2VtZQ==`
+    })
+  });
 
-	return fetch(request)
-		.then(res => res.json());
+  return fetch(request)
+    .then(res => res.json());
 };
 
 
 const searchHandler = function (request, reply) {
-	const input = request.payload.input;
-	reply(getData(input).then(res => res));
+  const input = request.payload.input;
+  reply(getData(input).then(res => res));
 };
 const acHandler = function (request, reply) {
-	const input = request.payload.input;
-	reply(getAc(input).then(res => res));
+  const input = request.payload.input;
+  reply(getAc(input).then(res => res));
 };
 
 const searchConfig = {
-	handler: searchHandler,
-	validate: {
-		payload: {
-			input: Joi.string().min(1).required()
-		}
-	}
+  handler: searchHandler,
+  validate: {
+    payload: {
+      input: Joi.string().min(1).required()
+    }
+  }
 };
 
 const acConfig = {
-	handler: acHandler,
-	validate: {
-		payload: {
-			input: Joi.string().min(1).required()
-		}
-	}
+  handler: acHandler,
+  validate: {
+    payload: {
+      input: Joi.string().min(1).required()
+    }
+  }
 };
 
 if (!module.parent) {
-	server.start((err) => {
+  server.start((err) => {
 
-		if (err) {
-			throw err;
-		}
+    if (err) {
+      throw err;
+    }
 
-		server.register(plugins, (err) => {
+    server.register(plugins, (err) => {
 
-			if (err) {
-				throw err;
-			}
+      if (err) {
+        throw err;
+      }
 
 
-			server.route([
-				{
-					method: 'GET',
-					path: '/',
-					handler: function (request, reply) {
-						reply.file(index);
-					},
-				},
-				{
-					method: 'POST',
-					path: '/search',
-					config: searchConfig
-				},
-				{
-					method: 'POST',
-					path: '/ac',
-					config: acConfig
-				}
-			]);
+      server.route([
+        {
+          method: 'GET',
+          path: '/',
+          handler: function (request, reply) {
+            reply.file(index);
+          },
+        },
+        {
+          method: 'POST',
+          path: '/search',
+          config: searchConfig
+        },
+        {
+          method: 'POST',
+          path: '/ac',
+          config: acConfig
+        }
+      ]);
 
-		});
+    });
 
-		console.log(`Server running at: ${server.info.uri}`);
-	});
+    console.log(`Server running at: ${server.info.uri}`);
+  });
 }
